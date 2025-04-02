11cf886e7239daa4616a0889a07c87463df79324
VictorFioravanteXavier <victor.fx0910@gmail.com>
2025-04-02 18:08:38 -0300

    (feature): Cadastrar contatos

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware')
+
 // Rotas da Home
 router.get('/', homeController.index);
 
@@ -10,5 +15,11 @@ router.get('/login/index', loginController.index)
 router.post('/login/register', loginController.register)
 router.post('/login/login', loginController.login)
 router.get('/login/logout', loginController.logout)
+
+// Rotas contato
+router.get('/contato/index', loginRequired, contatoController.index)
+router.post('/contato/register', loginRequired, contatoController.register)
+router.get('/contato/index/:id', loginRequired, contatoController.editIndex)
+
 
 module.exports = router;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,11 +1,11 @@
-exports.middlewareGlobal = function(req, res, next) {
+exports.middlewareGlobal = function (req, res, next) {
     res.locals.errors = req.flash('errors')
     res.locals.success = req.flash('success')
     res.locals.user = req.session.user;
     next()
 }
 
-exports.seuOutroMiddleware = function(req, res, next) {    
+exports.seuOutroMiddleware = function (req, res, next) {
     next()
 }
 
@@ -17,5 +17,17 @@ exports.checkCsrfError = (err, req, res, next) => {
 
 exports.csrfMidlleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken()
+    next()
+}
+
+exports.loginRequired = (req, res, next) => {
+    if (!req.session.user) {
+        req.flash("errors", "Você precisa fazer login.")
+        req.session.save(function () {
+            return res.redirect('/')
+        })
+        return
+    }
+
     next()
 }
